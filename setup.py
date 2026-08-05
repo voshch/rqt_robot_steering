beed68fee6a7d568c9840008a0419a6f9b32c483
Vova Shcherbyna <dev@voshch.dev>
2026-08-05 18:24:36 +0800

    icons!

--- setup.py
+++ setup.py
@@ -1,37 +1,36 @@
 from setuptools import setup
 
-package_name = 'rqt_robot_steering'
+package_name = "rqt_robot_steering"
 setup(
     name=package_name,
-    version='2.0.0',
-    package_dir={'': 'src'},
+    version="2.0.0",
+    package_dir={"": "src"},
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name + '/resource', ['resource/RobotSteering.ui']),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['plugin.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name + "/resource", ["resource/RobotSteering.ui", "resource/robot_steering_icon.png"]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["plugin.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    author='Dirk Thomas',
-    maintainer='Dirk Thomas',
-    maintainer_email='dthomas@osrfoundation.org',
-    keywords=['ROS'],
+    author="Dirk Thomas",
+    maintainer="Dirk Thomas",
+    maintainer_email="dthomas@osrfoundation.org",
+    keywords=["ROS"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
     ],
     description=(
-        'rqt_robot_steering provides a GUI plugin for steering a robot using Twist messages.'
+        "rqt_robot_steering provides a GUI plugin for steering a robot using Twist messages."
     ),
-    license='BSD',
+    license="BSD",
     entry_points={
-        'console_scripts': [
-            'rqt_robot_steering = ' + package_name + '.main:main',
-            ],
+        "console_scripts": [
+            "rqt_robot_steering = " + package_name + ".main:main",
+        ],
     },
 )
